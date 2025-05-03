0a46ae73a02536ab444812c8dcaaf168969bc908
Achyut Rimal <achyutrimal4@gmail.com>
2025-05-03 16:24:56 +0100

    complete project

--- greencycles_index_optimization_partitioning.sql
+++ greencycles_index_optimization_partitioning.sql
@@ -1,1 +1,86 @@
 -- USER MANAGEMENT
+
+CREATE USER achyut
+WITH password 'admin123'
+
+
+CREATE USER mike
+WITH PASSWORD 'mike123';
+
+
+CREATE ROLE read_only;
+CREATE ROLE read_update;
+
+
+GRANT USAGE 
+ON SCHEMA public
+TO read_only;
+
+
+-- Grant select on tables
+GRANT SELECT
+ON ALL TABLES IN SCHEMA public
+TO read_only; 
+
+
+GRANT read_only to mike;
+
+-- INDEXES
+SELECT(
+	SELECT AVG(amount)
+	FROM payment p2
+	WHERE p2.rental_id = p1.rental_id
+)
+FROM payment p1
+
+
+
+
+CREATE INDEX index_rental_id_payment
+ON payment
+(rental_id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
